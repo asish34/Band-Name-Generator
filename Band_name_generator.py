77bfa172002cd511b84b17ab0f2cea7db8c5c9e4
asish34 <63844544+asish34@users.noreply.github.com>
2021-10-03 11:30:43 +0530

    Update Band_name_generator.py
    Hi plz change this

--- Band_name_generator.py
+++ Band_name_generator.py
@@ -1,7 +1,6 @@
 print("Welcome to Band Name Generator!!")
 print("Enter the name of the city that you grew in")
-name = input()
+asish= input()
 print("Enter your pet name")
 pet = input()
-
-print("Your band name could be"+name+" "+pet)
+print(asish+" "+pet)
